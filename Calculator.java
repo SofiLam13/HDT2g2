c140368a02f4d0181c03f27fdfd2df573ace2c8a
SofiLam13 <87708580+SofiLam13@users.noreply.github.com>
2022-01-31 16:25:55 -0600

    Creación del Main y de las interfaces

---
+++ Calculator.java
@@ -0,0 +1,20 @@
+/**
+ * Universidad del Valle de Guatemala 
+ * Hoja de Trabajo 2
+ * Grupo 2
+ * Integrantes del grupo:
+ * Sofía Lam 21548
+ * Dáriel Villatoro
+ * Seongujun An 
+ * Defines all the methods that a Calculator should have
+ */
+public interface Calculator {
+    /**
+     * Calculates an entire operation.
+     * @param operation String containing the operation to be calculated.
+     * @return double with the result of the operation
+     */
+    public double calculate(String operation);
+}
+    
+
---
+++ Maingrupo2.java
@@ -0,0 +1,17 @@
+/**
+ * Universidad del Valle de Guatemala 
+ * Hoja de Trabajo 2
+ * Grupo 2
+ * Integrantes del grupo:
+ * Sofía Lam 21548
+ * Dáriel Villatoro
+ * Seongujun An 
+ */
+public class Maingrupo2{
+    
+    public static void main(String args[]) { 
+
+    }   
+    
+    
+}
