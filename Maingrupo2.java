69d6d13358c30be7e2f234d281b0e0c27d8bfd6a
SofiLam13 <87708580+SofiLam13@users.noreply.github.com>
2022-02-01 08:01:54 -0600

    Clase encargada de leer el archivo .txt

--- Maingrupo2.java
+++ Maingrupo2.java
@@ -7,11 +7,36 @@
  * Dáriel Villatoro
  * Seongujun An 
  */
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Maingrupo2{
     
     public static void main(String args[]) { 
 
     }   
-    
+    class FileReader{
+        
+        public ArrayList<String> archivo(){
+            ArrayList<String> expresion= new ArrayList<String>();
+            try{
+                String nombre= "datos.txt";
+                File myFile = new File(nombre);
+                Scanner s=new Scanner(myFile);
+                String linea="";
+                while (s.hasNextLine()){
+                    linea=s.nextLine();
+                    expresion.add(linea);
+                }
+                
+            }catch(Exception e){
+                    String m="FileReader: leer archivo():"+ e.getMessage();
+                    throw new RuntimeException(m);
+
+            }
+            return expresion;
+        }
+    }
     
 }
